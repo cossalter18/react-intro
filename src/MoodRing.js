363b5eeb9db3c81f4f2e829315d635bf552ed80e
Mike Cossalter <mikecossalter@yahoo.com>
2020-04-27 15:59:05 -0500

    first commit

---
+++ src/MoodRing.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+class MoodRing extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello from inside MoodRing render!!!</h1>
+                {/* comments work in here */}
+                {3 * 9}
+            </div>
+        ); // end return JSX
+    }// end render
+} //end component
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default MoodRing;
